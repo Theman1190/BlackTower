3f3cf5ab5f3c672704a828ade8f142b6fef0d848
Theman1190 <gman.peacock@gmail.ccom>
2013-03-09 20:30:00 -0800

    Added some basic edits to BlackTower.java

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-BlackTower    
+BlackTower
 ============
 Welcome!
 Thanks for checking out my first MineCraft mod!
 If you find any bugs please report it as a issue.
-
-This mod uses Minecraft Forge, Check it out here: http://www.minecraftforge.net/forum/
-
--- common/BlackTower.java
+++ common/BlackTower.java
@@ -1,4 +1,11 @@
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 
@@ -6,4 +13,22 @@ import cpw.mods.fml.common.network.NetworkMod;
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class BlackTower{
 	
+	@Instance("Generic")
+    public static BlackTower instance;
+	
+	@PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+            
+    }
+	
+	@Init
+    public void load(FMLInitializationEvent event) {
+           // proxy.registerRenderers();
+    }
+    
+	@PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+            
+    }
+
 }
