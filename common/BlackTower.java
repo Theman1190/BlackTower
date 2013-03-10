aed4b2bbb5d7b23d47fece73d1a83d4431a22400
Theman1190 <gman.peacock@gmail.ccom>
2013-03-09 16:10:15 -0800

    Renamed lmm.java to BlackTower.java

---
+++ common/BlackTower.java
@@ -0,0 +1,9 @@
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+
+@Mod(modid = "LMM", name = "BlackTower", version = "0.0.0_1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class BlackTower{
+	
+}
